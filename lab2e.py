142f1f62b3a49db489a7ca5c6041ed0a84032724
Ritika <rtamatta@myseneca.ca>
2026-05-19 16:25:06 -0400

    Completed Lab 2

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Author: Ritika Tamatta
+# Author ID: rtamatta
+# Date Created: 2026/05/19
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
